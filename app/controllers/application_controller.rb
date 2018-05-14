d4ecd7b77248e667d3569d65e41677e72b136f76
Hazel Wu <tinazx056@gmail.com>
2018-05-15 00:34:44 +0800

    Set up admin authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert] = "未有權限，不允許登入"
+      redirect_to root_path
+    end
+  end
 end
