e15a64de31a67689fa2d7e470df697fba167e9ca
Hazel Wu <tinazx056@gmail.com>
2018-05-15 00:44:55 +0800

    Add columns to restaurants table

---
+++ db/migrate/20180514164233_add_column_to_restaurants.rb
@@ -0,0 +1,9 @@
+class AddColumnToRestaurants < ActiveRecord::Migration[5.1]
+  def change
+    add_column :restaurants, :name, :string
+    add_column :restaurants, :tel, :string
+    add_column :restaurants, :address, :string
+    add_column :restaurants, :opening_hours, :string
+    add_column :restaurants, :description, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180514162339) do
+ActiveRecord::Schema.define(version: 20180514164233) do
 
   create_table "restaurants", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "tel"
+    t.string "address"
+    t.string "opening_hours"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
