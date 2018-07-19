d4bfe80bf5de38e3e2d59c89a210d21d627aebc9
Hazel Wu <tinazx056@gmail.com>
2018-07-19 17:44:23 +0800

    Setup restaurant resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
 
+  # 前台使用者權限
+  resources :restaurants, only: [:index, :show]
+
+  # 後台使用者權限
   namespace :admin do
     resources :restaurants
     resources :categories
