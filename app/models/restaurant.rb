7fa2b9e0d2445b0ea71200506a33c973254d881b
Hazel Wu <tinazx056@gmail.com>
2018-07-26 16:39:52 +0800

    Create Favorite model & setup reloation among user,  restaurant and favorite

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,10 @@ class Restaurant < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :category
 
+  # 收藏餐廳 多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
+  
   # CarrierWave 上傳圖片
   mount_uploader :image, PhotoUploader
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
         :recoverable, :rememberable, :trackable, :validatable
 
   # 如果 User 已經有了評論，就不允許刪除帳號（刪除時拋出 Error）
+  # 使用者可評論很多餐廳 多對多關聯
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments, dependent: :destroy
+
+  # 使用者可收藏很多餐廳 多對多關聯
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
 
   # 頭像上傳
   mount_uploader :avatar, AvatarUploader
